9325f0d6f13c921f8e00d4bd8c6e27ebd3fbf9a1
Ioann Volkov <volkov.ioann@gmail.com>
2024-06-06 21:53:11 +0300

    Some safe space above and below the minitext, otherwise it's touching the stop button for locomotives without lights

--- App/app/src/main/java/ru/tsar_ioann/legotrainscontrol/ui/LocomotiveControls.kt
+++ App/app/src/main/java/ru/tsar_ioann/legotrainscontrol/ui/LocomotiveControls.kt
@@ -2,6 +2,7 @@ package ru.tsar_ioann.legotrainscontrol.ui
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +17,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.tsar_ioann.legotrainscontrol.Train
 
@@ -41,7 +43,7 @@ fun LocomotiveControls(locomotive: Train.Locomotive, onLightsChanged: (Float) ->
             textAlign = TextAlign.Right,
             fontSize = 8.sp,
             lineHeight = 12.sp,
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.weight(1f).padding(vertical = 12.dp),
         )
     }
 }
