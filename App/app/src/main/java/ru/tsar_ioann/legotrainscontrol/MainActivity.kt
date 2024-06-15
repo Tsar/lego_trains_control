1044d85b876dcba18378e1dab4f0313a7c227521
Ioann Volkov <volkov.ioann@gmail.com>
2024-06-15 19:05:08 +0300

    Add action bar with 2 action buttons (approach is copied from Smart Home project), they don't do anything yet

--- App/app/src/main/java/ru/tsar_ioann/legotrainscontrol/MainActivity.kt
+++ App/app/src/main/java/ru/tsar_ioann/legotrainscontrol/MainActivity.kt
@@ -21,6 +21,8 @@ import android.os.Build
 import android.os.Bundle
 import android.os.ParcelUuid
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -94,7 +96,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Main(
                 UIData(
@@ -109,6 +110,11 @@ class MainActivity : ComponentActivity() {
         }
 
         discoverTrains()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 
     override fun onDestroy() {
@@ -159,6 +165,10 @@ class MainActivity : ComponentActivity() {
             bleScanner?.stopScan(scanCallback)
         }
     }
+
+    fun onAddNewTrain(menuItem: MenuItem) {}
+
+    fun onSetup(menuItem: MenuItem) {}
 
     private fun onRedWarningBoxClick() {
         when (redWarning.value) {
