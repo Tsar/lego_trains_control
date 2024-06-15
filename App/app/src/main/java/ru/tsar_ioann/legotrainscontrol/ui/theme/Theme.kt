1a65a0afe7c571a76ff69cb6070480b646e820fe
Ioann Volkov <volkov.ioann@gmail.com>
2024-06-15 19:05:08 +0300

    Add action bar with 2 action buttons (approach is copied from Smart Home project), they don't do anything yet

--- App/app/src/main/java/ru/tsar_ioann/legotrainscontrol/ui/theme/Theme.kt
+++ App/app/src/main/java/ru/tsar_ioann/legotrainscontrol/ui/theme/Theme.kt
@@ -1,27 +1,10 @@
 package ru.tsar_ioann.legotrainscontrol.ui.theme
 
-import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun LegoTrainsControlTheme(
@@ -30,7 +13,7 @@ fun LegoTrainsControlTheme(
     dynamicDarkColorScheme(LocalContext.current)
 
     MaterialTheme(
-        colorScheme = DarkColorScheme,
+        colorScheme = darkColorScheme(),
         typography = Typography,
         content = content
     )
